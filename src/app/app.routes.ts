eecf7f234af6846d1f6f1f87ba06b8534ed34b6f
tina <tinarezanezhaad@gmail.com>
2025-08-20 17:02:48 +0200

    Deploy Angular app

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { BlogComponent } from './components/blog/blog.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'blog', component: BlogComponent },
-  { path: '**', redirectTo: '' },
-];
